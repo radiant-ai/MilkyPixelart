705a97eeb9a809b226cd460ec296135c55ee2d30
radiant-ai <bodia1206@gmail.com>
2022-02-05 22:05:03 -0600

    v.1.3.2 removed ownedblocks api

--- src/main/java/fun/milkyway/milkypixelart/ownedblocks/OwnedBlocksPDCException.java
+++
@@ -1,7 +0,0 @@
-package fun.milkyway.milkypixelart.ownedblocks;
-
-public class OwnedBlocksPDCException extends Exception{
-    protected OwnedBlocksPDCException(String message) {
-        super(message);
-    }
-}
