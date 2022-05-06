5560eb1d6c572c9a7b63a870d9f3539d90cbff58
radiant-ai <bodia1206@gmail.com>
2022-05-05 19:43:23 -0500

    preview only if even got cancelled

--- src/main/java/fun/milkyway/milkypixelart/listeners/AuctionPreviewListener.java
+++ src/main/java/fun/milkyway/milkypixelart/listeners/AuctionPreviewListener.java
@@ -3,11 +3,7 @@ package fun.milkyway.milkypixelart.listeners;
 import fun.milkyway.milkypixelart.MilkyPixelart;
 import fun.milkyway.milkypixelart.managers.PixelartManager;
 import fun.milkyway.milkypixelart.utils.Utils;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,27 +12,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.UUID;
-
 public class AuctionPreviewListener implements Listener {
 
-    @EventHandler(priority = EventPriority.LOWEST)
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onRightClick(InventoryClickEvent event) {
         if (event.getClick().isRightClick() &&
                 event.getClickedInventory() != null &&
                 event.getCurrentItem() != null &&
-                event.getCurrentItem().getType().equals(Material.FILLED_MAP)) {
+                event.getCurrentItem().getType().equals(Material.FILLED_MAP) &&
+                event.isCancelled() &&
+                event.getWhoClicked() instanceof Player player) {
             String invName = PlainTextComponentSerializer.plainText().serialize(event.getView().title());
-            if ((MilkyPixelart.getInstance().getConfiguration().getBoolean("pixelarts.previewEverywhere", false) ||
-                    Utils.containsAny(invName, MilkyPixelart.getInstance().getConfiguration().getStringList("pixelarts.previewInventories")))
-                    && event.getWhoClicked() instanceof Player player) {
+            if (MilkyPixelart.getInstance().getConfiguration().getBoolean("pixelarts.previewEverywhere", false)
+                    ||  Utils.containsAny(invName, MilkyPixelart.getInstance().getConfiguration().getStringList("pixelarts.previewInventories"))) {
 
                 ItemStack stack = event.getCurrentItem();
 
                 PixelartManager.getInstance().renderArt(player, stack);
 
                 player.closeInventory();
-                event.setCancelled(true);
             }
         }
     }
