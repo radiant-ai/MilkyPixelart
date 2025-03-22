2f50b395b7f85be1f8496ee96a31d7ee8a1fc6ea
radiant-ai <bodia1206@gmail.com>
2025-03-21 23:34:46 -0500

    rollback usage of older api, not feasible for me to support

--- src/main/java/fun/milkyway/milkypixelart/utils/MaterialUtils.java
+++ src/main/java/fun/milkyway/milkypixelart/utils/MaterialUtils.java
@@ -4,22 +4,6 @@ import org.bukkit.Material;
 
 public class MaterialUtils {
     public static boolean isBundle(Material material) {
-        return material == Material.BUNDLE ||
-                material == Material.BLACK_BUNDLE ||
-                material == Material.BLUE_BUNDLE ||
-                material == Material.BROWN_BUNDLE ||
-                material == Material.CYAN_BUNDLE ||
-                material == Material.GRAY_BUNDLE ||
-                material == Material.GREEN_BUNDLE ||
-                material == Material.LIGHT_BLUE_BUNDLE ||
-                material == Material.LIGHT_GRAY_BUNDLE ||
-                material == Material.LIME_BUNDLE ||
-                material == Material.MAGENTA_BUNDLE ||
-                material == Material.ORANGE_BUNDLE ||
-                material == Material.PINK_BUNDLE ||
-                material == Material.PURPLE_BUNDLE ||
-                material == Material.RED_BUNDLE ||
-                material == Material.WHITE_BUNDLE ||
-                material == Material.YELLOW_BUNDLE;
+        return material.name().endsWith("BUNDLE");
     }
 }
